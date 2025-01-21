20080e5c2a2736f7ebf9777d687c4cbf4a458845
judah <judahjosh@gmail.com>
2025-01-21 09:51:19 -0500

    modified:   challenge01/solution.cpp

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -7,14 +7,26 @@
 #include <cstdlib>
 #include <array>
 using namespace std;
-int storearray(){
 
+int storearray(){
+    
     return 0;
 
 }
 
+// rotate the array, by d elements, in the direction d
 int rotatearray(vector<int> arr, int d){
     int n = arr.size();
+
+if (char direct = 'L') {
+    d = n - d;
+
+}
+if (char direct = 'R')
+{
+    d = n - d;
+
+}
 
     int d = d % n;
     vector<int> temp(d);
@@ -27,19 +39,24 @@ int rotatearray(vector<int> arr, int d){
 }
 
 int printarray(){
+    int n;
+    vector<int> arr(n);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
 
-// int readarray(){
-//     int n;
-//     vector<int> arr(n);
-//     cin >> n;
-//     for (int i = 0; i < n; i++) {
-//         cin >> arr[i];
-//     }
-//     return 0;
-// }
-
+int readarray(){
+    int n;
+    vector<int> arr(n);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    return 0;
+}
 int main(int argc, char *argv[]) {
     int n;
     vector<int> arr(n);
@@ -49,4 +66,13 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
+// int main(int argc, char *argv[]) {
+//     int n;
+//     vector<int> arr(n);
+//     cin >> n;
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     return 0;
+// }
 
