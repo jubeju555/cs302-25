100041f467ee390bbdf977d0e90fcb6e9f639ee0
judah <judahjosh@gmail.com>
2025-01-21 09:40:45 -0500

    modified:   challenge01/solution.cpp

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -14,16 +14,15 @@ int storearray(){
 }
 
 int rotatearray(vector<int> arr, int d){
-   int n = arr.size();
+    int n = arr.size();
 
     int d = d % n;
-
     vector<int> temp(d);
 
     for (int i = 0; i < d; i++) {
         temp[i] = arr[i];
     }
-    
+
     return 0;
 }
 
@@ -31,17 +30,23 @@ int printarray(){
     return 0;
 }
 
-int readarray(){
+// int readarray(){
+//     int n;
+//     vector<int> arr(n);
+//     cin >> n;
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     return 0;
+// }
+
+int main(int argc, char *argv[]) {
     int n;
     vector<int> arr(n);
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
     return 0;
 }
 
