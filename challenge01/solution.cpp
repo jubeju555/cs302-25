e93f23f55f0ab90d26a5ed7fb1e3e5966966c3ff
judah <judahjosh@gmail.com>
2025-01-21 08:25:16 -0500

    modified:   challenge01/solution.cpp

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -1,4 +1,33 @@
 // Challenge 01: Rotating Arrays; null solution
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <cstdlib>
+int storearray(){
+
+    return 0;
+
+}
+
+int rotatearray(){
+
+    return 0;
+}
+
+int printarray(){
+    return 0;
+}
+
+int readarray(){
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     return 0;
