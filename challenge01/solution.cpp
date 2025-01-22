321a025e1f228607295ce58c5de343f4978d7d02
judah <judahjosh@gmail.com>
2025-01-22 09:22:30 -0500

    new file:   challenge01/challenge01.tar 	modified:   challenge01/solution 	modified:   challenge01/solution.cpp

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -1,6 +1,6 @@
 // Challenge 01: Rotating Arrays; null solution
 /*
-judah benjamin 
+judah benjamin
 cs302
 challenge 1
 this program takes an array and shifts it a certian amount left or right depending on the input*/
@@ -14,32 +14,43 @@ this program takes an array and shifts it a certian amount left or right dependi
 #include <array>
 using namespace std;
 
-int storearray(){
+int storearray()
+{
     return 0;
 }
 // rotate the array, by d elements, in the direction d
-int rotatearray(vector<int> arr, int d, char Dir){
-    int n = arr.size();
-if (Dir == 'R') d = n - d;
-    d = d % n;
-    cout << "index: " << d << endl;
- for (int i = 0; i < n; i++) {
-        cout << arr[(i + d) % n] << " ";
+int rotatearray(vector<int> arr, int index, char Dir)
+{
+    int size= arr.size();
+    if (Dir == 'R')
+       index= size-index;
+    if (index < 5)
+    {
+       index+= 5;
+    }
+
+   index = index % size;
+    // cout << "index: " << d << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[(i + index) % size] << " ";
     }
 
     cout << endl;
     return 0;
 }
-int main(int argc, char *argv[]) {
-    int n, d; 
+int main(int argc, char *argv[])
+{
+    int size, index;
     char Dir;
-    while (cin >> n >> d >> Dir) {
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
- 
+    while (cin >> size >> index >> Dir)
+    {
+        vector<int> arr(size);
+        for (int i = 0; i < size; i++)
+        {
             cin >> arr[i];
         }
-       rotatearray(arr, d, Dir);
+        rotatearray(arr, index, Dir);
     }
     return 0;
 }
