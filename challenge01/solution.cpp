4fcbcf9e7cf5c20d183545434988ee97d140d3ab
judah <judahjosh@gmail.com>
2025-01-22 09:11:23 -0500

    modified:   challenge01/.vscode/settings.json 	new file:   challenge01/.vscode/tasks.json 	new file:   challenge01/solution 	modified:   challenge01/solution.cpp

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -1,77 +1,45 @@
 // Challenge 01: Rotating Arrays; null solution
+/*
+judah benjamin 
+cs302
+challenge 1
+this program takes an array and shifts it a certian amount left or right depending on the input*/
 #include <iostream>
 #include <vector>
 #include <string>
 #include <sstream>
 #include <algorithm>
 #include <cstdlib>
+#include <cstdio>
 #include <array>
 using namespace std;
 
 int storearray(){
     return 0;
-
 }
-
 // rotate the array, by d elements, in the direction d
-int rotatearray(vector<int> arr, int d){
+int rotatearray(vector<int> arr, int d, char Dir){
     int n = arr.size();
-
-if (char direct = 'L') {
-    d = n - d;
-
-}
-if (char direct = 'R')
-{
-    d = n - d;
-
-}
-
-    int d = d % n;
-    vector<int> temp(d);
-
-    for (int i = 0; i < d; i++) {
-        temp[i] = arr[i];
+if (Dir == 'R') d = n - d;
+    d = d % n;
+    cout << "index: " << d << endl;
+ for (int i = 0; i < n; i++) {
+        cout << arr[(i + d) % n] << " ";
     }
 
+    cout << endl;
     return 0;
 }
-
-// int printarray(){
-//     int n;
-//     vector<int> arr(n);
-//     cin >> n;
-//     for (int i = 0; i < n; i++) {
-//         cout << arr[i] << " ";
-//     }
-//     return 0;
-// }
-
-// int readarray(){
-//     int n;
-//     vector<int> arr(n);
-//     cin >> n;
-//     for (int i = 0; i < n; i++) {
-//         cin >> arr[i];
-//     }
-//     return 0;
-// }
 int main(int argc, char *argv[]) {
-    int n;
-    vector<int> arr(n);
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    int n, d; 
+    char Dir;
+    while (cin >> n >> d >> Dir) {
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+ 
+            cin >> arr[i];
+        }
+       rotatearray(arr, d, Dir);
     }
     return 0;
 }
-// int main(int argc, char *argv[]) {
-//     int n;
-//     vector<int> arr(n);
-//     cin >> n;
-//     for (int i = 0; i < n; i++) {
-//         cin >> arr[i];
-//     }
-//     return 0;
-// }
-
