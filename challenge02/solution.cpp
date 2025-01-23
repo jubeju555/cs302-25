388a7957f88592b5fbe466ec52dca2c9e9d975d4
judah <judahjosh@gmail.com>
2025-01-23 13:45:54 -0500

    Co-authored-by: Keatonium <Keatonium@users.noreply.github.com>

--- challenge02/solution.cpp
+++ challenge02/solution.cpp
@@ -15,6 +15,33 @@ using namespace std;
 
 // Main Execution
 
+int findclosest(){
+    int n;
+    cin >>n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+    int smalldiff = INT_MAX;
+    // as you sort the array, the smallest difference will be between adjacent elements
+    for (int i = 0; i < n-1; i++)
+    {
+        smalldiff = min(smalldiff, arr[i+1]-arr[i]);
+    }
+
+    for (int i = 0; i < n-1; i++)
+    {
+        if(arr[i+1]-arr[i] == smalldiff){
+            cout << arr[i] << " " << arr[i+1] << " ";
+        }
+    }
+
+}
+
 int main(int argc, char *argv[]) {
+    findclosest();
     return EXIT_SUCCESS;
 }
