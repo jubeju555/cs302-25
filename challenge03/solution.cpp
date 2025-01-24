e4614150df89a5ebf80e84c68ca67ae2a3e08135
judah <judahjosh@gmail.com>
2025-01-24 06:57:42 -0500

    modified:   challenge03/solution.cpp

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -1,4 +1,22 @@
 // Challenge 03: Palindrome Permutation
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+bool is_palindrome_permutation(const string &s) {
+  // using a map to store and check how oftern char repetition happens
+  unordered_map<char, int> num_char;
+  for (char c : s)
+  {
+    num_char++;
+  }
+
+  
+  return (false);
+}
+
+
 
 int main(int argc, char *argv[]) {
   return (0);
