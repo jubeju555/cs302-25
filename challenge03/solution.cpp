ea6779f919fd31db6afd5918ad25c74bfc3a6101
judah <judahjosh@gmail.com>
2025-01-28 17:32:14 -0500

    new file:   challenge03/challenge03.tar 	modified:   challenge03/solution 	modified:   challenge03/solution.cpp

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -1,49 +1,43 @@
-// Challenge 03: Palindrome Permutation
-/*
-judah benjamin
-this code checks whether a string is a palindrom permutation*/
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <cctype>
 using namespace std;
-// use unordered map to store num of chars in each sequaence, then mod by 2 to check if palindrome
-bool is_palindrome_permutation(const string &readline)
-{
-    int permcount = 0;
-  
-  unordered_map<char, int> num_char;
-  for (char c : readline)
-  {
-    num_char[c]++;
-  }
-// if num of chars is odd, increment permcount
-  for (string ::const_iterator it = readline.begin(); it != readline.end(); it++)
-  {
 
-    if (num_char[*it] % 2 != 0)
-    {
-      permcount++;
+bool is_palindrome_permutation(const string &readline) {
+    unordered_map<char, int> num_char;
+    
+    // count how many chars read
+    for (string::const_iterator it = readline.begin(); it != readline.end(); ++it) {
+      // looked up how to only look at letters, used site programiz, for info on isalpha
+        if (isalpha(*it)) {  
+            char c = tolower(*it);  
+            num_char[c]++;
+        }
     }
-    if (permcount < 1)
-    {
-      return (true);
+    
+    // check if more then 1 char has an odd count 
+    int permcount = 0;
+    for (unordered_map<char, int>::const_iterator it = num_char.begin(); it != num_char.end(); ++it) {
+        if (it->second % 2 != 0) {
+            permcount++;
+        }
+        if (permcount > 1) {
+            return false;
+        }
     }
-  }
-  return (false);
+    
+    return true;
 }
-int main(int argc, char *argv[])
-{
-  string readline;
-  while (getline(cin, readline))
-  {
-    if (is_palindrome_permutation(readline) == true)
-    {
-      cout << "\"" << readline << "\"" << " is a palindrome permutation" << endl;
+
+int main(int argc, char *argv[]) {
+    string readline;
+    while (getline(cin, readline)) {
+        if (is_palindrome_permutation(readline)) {
+            cout << "\"" << readline << "\"" << " is a palindrome permutation" << endl;
+        } else {
+            cout << "\"" << readline << "\"" << " is not a palindrome permutation" << endl;
+        }
     }
-    else
-    {
-      cout << "\"" << readline << "\"" << " is not a palindrome permutation" << endl;
-    }
-  }
-  return (0);
+    return 0;
 }
