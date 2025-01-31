8ee6921733a94f464a04dbf87dbbf16bbe7c7615
judah <judahjosh@gmail.com>
2025-01-30 20:00:19 -0500

    new file:   challenge04/solution 	modified:   challenge04/solution.cpp

--- challenge04/solution.cpp
+++ challenge04/solution.cpp
@@ -1,10 +1,37 @@
 // Challenge 04: Graph Paths
-
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 // Main Execution
 
+
 int main(int argc, char *argv[]) {
+  unordered_map<string, vector<string>> graph;
+  string line;
+  while (getline(cin, line))
+  {
+    stringstream ss(line);
+    string node;
+    ss >> node;
+    string edge;
+    while (ss >> edge)
+    {
+      graph[node].push_back(edge);
+    }
+    if(graph.find(node) == graph.end())
+    {
+      graph[node] = {};
+    }else
+    {
+      graph[node].push_back("");
+    }
+  }
+
+  
   return (0);
 }
 
