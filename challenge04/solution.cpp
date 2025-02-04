b595e6fdd8922b0c66acfd5775910527c0e2f8eb
judah <judahjosh@gmail.com>
2025-02-04 09:36:07 -0500

    modified:   challenge04/solution.cpp

--- challenge04/solution.cpp
+++ challenge04/solution.cpp
@@ -7,44 +7,62 @@
 using namespace std;
 
 // Main Execution
-
-int main(int argc, char *argv[]) {
-  unordered_map<string, vector<string>> graph;
-  string line;
-  while (getline(cin, line))
+bool dfs(unordered_map<string, vector<string>> graph, string start, string end, vector<string> visited){
+  if (start == end)
   {
-    stringstream ss(line);
-    string node;
-    ss >> node;
-    string edge;
-    while (ss >> edge)
-    {
-      graph[node].push_back(edge);
-    }
-    if(graph.find(node) == graph.end())
-    {
-      graph[node] = {};
-    }else
-    {
-      graph[node].push_back("");
-    }
+    return true;
   }
-  while (cin >> line)
+  visited.push_back(start);
+  for (const string &node : graph[start])
   {
-    if(graph.find(line) == graph.end())
+  if (find(visited.begin(), visited.end(), node) == visited.end())
+  {
+    if (dfs(graph, node, end, visited))
     {
-      cout << "In Graph " << line << " there no path from " << node[1] << " to" << endl;
-    }else
-    {
-      cout << "In Graph " << line << " there a " << graph[line].size() << " paths" << endl;
+      return true;
     }
+
+  }
+
+  return true;
+}
+int main(int argc, char *argv[]) {
+//   ;
+//   string line;
+//   while (getline(cin, line))
+//   {
+//     stringstream ss(line);
+//     string node;
+//     ss >> node;
+//     string edge;
+//     while (ss >> edge)
+//     {
+//       graph[node].push_back(edge);
+//     }
+//     if(graph.find(node) == graph.end())
+//     {
+//       graph[node] = {};
+//     }else
+//     {
+//       graph[node].push_back("");
+//     }
+//   }
+//   while (cin >> line)
+//   {
+//     if(graph.find(line) == graph.end())
+//     {
+//       cout << "In Graph " << line << " there no path from " << node[1] << " to" << endl;
+//     }else
+//     {
+//       cout << "In Graph " << line << " there a " << graph[line].size() << " paths" << endl;
+//     }
    
 
-  }
+//   }
   
-cout << "graph size: " << graph.size() << endl;
-cout << "graph: " << endl;
-cout << "graph[0]: " << graph["0"].size() << endl;
+// cout << "graph size: " << graph.size() << endl;
+// cout << "graph: " << endl;
+// cout << "graph[0]: " << graph["0"].size() << endl;
   
   return (0);
 }
