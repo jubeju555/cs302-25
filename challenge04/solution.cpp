4ae03a84bf7fc0d91fc64ce7b586f8f4fb0cd7c5
judah <judahjosh@gmail.com>
2025-02-04 16:54:01 -0500

    modified:   challenge04/solution.cpp

--- challenge04/solution.cpp
+++ challenge04/solution.cpp
@@ -10,47 +10,57 @@ using namespace std;
 // Main Execution
 bool dfs(unordered_map<string, vector<string>> graph, string start, string end, vector<string> visited)
 {
-  if (start == end) return true;
-if (visited.begin() != visited.end()) return false;
+  if (start == end)
+    return true;
+  if (visited.begin() != visited.end())
+    return false;
   visited.push_back(start);
-
-
-
-
 
   for (const string &node : graph[start])
   {
     if (dfs(graph, node, end, visited))
-      {
-        return true;
-      }
+    {
+      return true;
+    }
   }
   return false;
 }
 
 int main(int argc, char *argv[])
 {
-  while(true){
-  int nedges;
-  if(!(cin >> nedges)) break;
-
-
   unordered_map<string, vector<string>> graph;
-
-  for (size_t i = 0; i < nedges; i++)
+  // NEDGES pairs of nodes where the first string is the source and the second string is the destination
+  int npaths;
+  while (true)
   {
-    string start, end;
-    cin >> start >> end;
-    graph[start].push_back(end);
-  }
+    int nedges;
+    if (!(cin >> nedges)){
+      break;
+    }
 
   
-  
+    for (size_t i = 0; i < nedges; i++)
+    {
+      string start, end;
+      cin >> start >> end;
+      graph[start].push_back(end);
+    }
+// NPATHS which is the number of paths or routes to search for
+    int npaths;
+    cin >> npaths;
+    for (size_t i = 0; i < npaths; i++)
+    {
+      string src,dst;
+      cin >> src >> dst;
+      graph[src].push_back(dst);
+    }
+    
+    
   }
   // unordered_map<string, vector<string>> graph;
   // vector<string> visited;
-  // string start = "A"; 
-  // string end = "B";  
+  // string start = "A";
+  // string end = "B";
   // int graphnum;
   // while (cin >> graphnum >> start >> end)
   // {
