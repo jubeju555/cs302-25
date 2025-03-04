8b26c0e30583d7f477ccd8d5f31dd7cf1958ed08
judah <judahjosh@gmail.com>
2025-03-03 22:00:26 -0500

    starting off

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -1,5 +1,16 @@
 // Challenge 06: Repeated DNA sequences
-
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+void DNAsequences(string s){
+unordered_map <string, int> m;
+vector <string> res;
+for(int i; i < 9; i++){
+    
+}
+}
 int main(int argc, char *argv[]) {
     return 0;
 }
