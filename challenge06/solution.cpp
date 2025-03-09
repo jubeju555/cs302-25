096c2ceccb0ad7c2f01cbfb97bbc5c9f0ffd353a
judah <judahjosh@gmail.com>
2025-03-09 19:25:18 -0400

    initial print out, kind of wrong

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -4,15 +4,31 @@
 #include <string>
 #include <unordered_map>
 using namespace std;
-void DNAsequences(string s){
-unordered_map <string, int> m;
-vector <string> res;
-for(int i; i < 9; i++){
-    
+
+void DNAsequences(string s) {
+    unordered_map<string, int> dnamap;
+    vector<string> DNA;
+    for (int i = 0; i <= s.size(); i++) {
+        string substring = s.substr(i, 9);
+        if (dnamap[substring] == 1) {
+            DNA.push_back(substring);
+        }
+        dnamap[substring]++;
+    }
+    for (const string& seq : DNA) {
+        if (dnamap[seq] == 2)
+        {
+            cout << seq;
+        }
+      if(cin.eof())cout << endl;
+    }
+    cout << "-1" << endl;
 }
-}
+
 int main(int argc, char *argv[]) {
+    string s;
+    while (getline(cin, s)) {
+        DNAsequences(s);
+    }
     return 0;
 }
-
-
