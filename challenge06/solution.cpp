5f10c35a146c3956db446cebf66451a7fa9ea909
judah <judahjosh@gmail.com>
2025-03-09 20:14:40 -0400

    ok now its done

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -9,11 +9,16 @@ using namespace std;
 void DNAsequences(string s) {
     unordered_map<string, int> dnamap;
     vector<string> DNA;
-    if (s.size() < 9) cout << "-1" << endl; return;
+    if (s.size() < 9) {
+cout << "-1" << endl;
+        return;
+    }
+
     for (size_t i = 0; i <= s.size() - 9; i++) {
         string substring = s.substr(i, 9);
         dnamap[substring]++;
-        if (dnamap[substring] == 2) DNA.push_back(substring);   
+        if (dnamap[substring] == 2) DNA.push_back(substring);
+        
     }
     if(DNA.empty()){
         printf("-1\n");
