3c0d5738d75b3e86ee8ded52949a6e2a051adce1
judah <judahjosh@gmail.com>
2025-03-25 11:44:29 -0400

    just checking if normal version is working

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -1,11 +1,16 @@
 // Challenge 06: Repeated DNA sequences
+/*
+judah benjamin
+challege 6 
+dna sequaneces 
+this program sort adn spits out orgnaizes dna sequesnces from long strands of info */
 #include <iostream>
 #include <vector>
 #include <string>
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
-
+// function based approach
 void DNAsequences(string s) {
     unordered_map<string, int> dnamap;
     vector<string> DNA;
@@ -13,6 +18,7 @@ void DNAsequences(string s) {
 cout << "-1" << endl;
         return;
     }
+    // count 9 letter strings, add if repeated
     for (size_t i = 0; i <= s.size() - 9; i++) {
         string substring = s.substr(i, 9);
         dnamap[substring]++;
@@ -22,18 +28,19 @@ cout << "-1" << endl;
     if(DNA.empty()){
         printf("-1\n");
     }else{
+        // sort lexicographically
     sort(DNA.begin(), DNA.end());
     for (const string& seq : DNA) {
             cout << seq << endl;
         }
         printf("-1\n");
-    }
-        
+    }    
     
 }
 
 int main(int argc, char *argv[]) {
     string s;
+    // getline cause its cleaner 
     while (getline(cin, s)) {
         DNAsequences(s);
     }
--- proj4/src/dijkstras.cpp
+++ proj4/src/dijkstras.cpp
@@ -1,11 +1,122 @@
-// dijsktras.cpp
+// dijkstra's.cpp
 
 // Main Execution
 
 #include <iostream>
 #include <vector>
 #include <queue>
-int main(int argc, char *argv[]) {
-    return 0;
+#include <utility>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <climits>
+
+
+
+using namespace std;
+// need something to store weights
+/*and need something to store graph - lets use a priority queue, since the smallest weight will just be at the top
+we can use like a min heap and keep finding the smallest way to a encompass the "map"
+*/
+// pretty close to geeks4geeks might need to change to queue
+struct graph {
+    int weight;
+    int node;
+    int v;
+    set<int> visited;
+    vector<int> dist;
+    int mindist();
+    int dijkstrasalgo();
+};
+
+int graph::mindist()
+{
+    
+    int min = INT_MAX, min_index;
+    int verti = 0;
+    for (int i = 0; i < verti; i++)
+    {
+        if (visited.find(i) == visited.end() && dist[i] <= min)
+        {
+            min = dist[i];
+            min_index = i;
+        }
+    }
+    return min_index;
 }
 
+
+void printSolution(int dist[], int n)
+{
+    int v = n;
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < v; i++)
+        printf("\t%d \t\t\t\t %d\n", i, dist[i]);
+}
+
+int graph::dijkstrasalgo()
+{
+    int weight = 0;
+    int start = 0;
+    int end = 0;
+    priority_queue<int> minqueue;
+
+    // store shortest path somewhere in an int
+    // use a bool to check if the node is the shortest path
+    // use mindist function to actually find the shortest path
+    // update the distances of the nodes
+    // use a priority queue to store the weights
+    
+    // Initialize distances to all vertices as infinite and distance to the source as 0
+    dist.resize(v, INT_MAX);
+    dist[start] = 0;
+
+    // Find shortest path for all vertices
+    for (int count = 0; count < v - 1; count++)
+    {
+        // Pick the minimum distance vertex from the set of vertices not yet processed
+        int u = mindist();
+        visited.insert(u);
+
+        // Update dist value of the adjacent vertices of the picked vertex
+        for (int i = 0; i < v; i++)
+        {   // double check if this is correct
+            // Update dist[i] if it is not in visited, there is an edge from u to i,
+            // and total weight of path from start to i through u is smaller than current value of dist[i]
+            if (!visited.count(i) && dist[u] != INT_MAX && dist[u] + weight < dist[i])
+            {
+                dist[i] = dist[u] + weight;
+            }
+        }
+    }
+
+    // Print the constructed distance array
+    printSolution(dist.data(), v);
+
+    
+}
+int main(int argc, char *argv[])
+{
+    graph g;
+    int weight, n;
+    int graphx, graphy;
+    while (cin >> n)
+    {
+        // dijsktrasalgo(n);
+        // cout << "Enter the number of nodes: " << n << endl;
+        // reads in number of types and store the weight of each node
+        cin >> n;
+        vector<pair<char, int>> nodes;
+        for (int i = 0; i < n; ++i)
+        {
+            char node;
+            int weight;
+            cin >> node >> weight;
+            nodes.push_back(make_pair(node, weight));
+            
+        }
+        cin >> graphx >> graphy;
+        g.dijkstrasalgo();
+    }
+    return 0;
+}
