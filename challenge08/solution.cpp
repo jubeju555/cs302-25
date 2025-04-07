9be1307a9c28a94e1d1989d8afa0196f9b3aea76
judah <judahjosh@gmail.com>
2025-04-07 18:25:08 -0400

    no output,

--- challenge08/solution.cpp
+++ challenge08/solution.cpp
@@ -6,34 +6,54 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+// int grid(int n,int row, int col) {
 
-map<int, int> memo;
+//     vector<vector<int>> grid;
+//     grid.resize(row);
+//     for (int i = 0; i < row; i++) {
+//         grid[i].resize(col);
+//     }
+//     // Initialize the grid with zeros
+//     // Base cases
 
+//     if (n <= 0) return 0;
+//     if (n >= 1) return 1;
+// }
+int calcscore(string str1, string str2)
+{
 
-int fib(int n,int row, int col) {
+    int col = str1.length() + 1;
+    int row = str2.length() + 1;
 
-    int grid[row][col];
-    // Base cases
-    if (n < 0) return 0;   
-    if (n >= 1) return 1;    
-    
+    vector<vector<int>> score;
+    // initiliaze matrix
+    for (int i = 0; i < row; i++)
+        score[i][0] = -i;
+    for (int j = 0; j < col; j++)
+        score[0][j] = -j;
 
-    if (memo.find(n) != memo.end()) return memo[n];
+    // recursive function
+    for (int i = 1; i < row; i++)
+    {
+        for (int j = 1; j < col; j++)
+        {
+            // is match = 1, if not = -1
+            int match = (str2[i - 1] == str1[j - 1]) ? 1 : -1;
+            // score for diag, up, left
+            score[i][j] = max({score[i - 1][j - 1 + match], score[i - 1][j] - 1, score[i][j - 1] - 1} );
+        }
+    }
 
-
-    int result = 0;
-
-
-    return memo[n];
+    return score[row - 1][col - 1];
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     string str1;
     string str2;
     cin >> str1;
     cin >> str2;
+    int score = calcscore(str1, str2);
+    cout << score << endl;
     return 0;
 }
-
-
