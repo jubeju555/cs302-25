bea5a305db803dfabca947299e8b48e6a10db40c
judah <judahjosh@gmail.com>
2025-04-07 20:27:06 -0400

    done

--- challenge08/solution.cpp
+++ challenge08/solution.cpp
@@ -6,31 +6,19 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-// int grid(int n,int row, int col) {
-
-//     vector<vector<int>> grid;
-//     grid.resize(row);
-//     for (int i = 0; i < row; i++) {
-//         grid[i].resize(col);
-//     }
-//     // Initialize the grid with zeros
-//     // Base cases
-
-//     if (n <= 0) return 0;
-//     if (n >= 1) return 1;
-// }
 int calcscore(string str1, string str2)
 {
-
     int col = str1.length() + 1;
     int row = str2.length() + 1;
-
     vector<vector<int>> score;
-    // initiliaze matrix
-    for (int i = 0; i < row; i++)
-        score[i][0] = -i;
-    for (int j = 0; j < col; j++)
-        score[0][j] = -j;
+    score.resize(row);
+    
+    for (int i = 0; i < row; ++i) {
+        score[i].resize(col, 0);
+    }    
+    // initialize matrix, basically set (0,0) up to (row, col)
+    for (int i = 0; i < row; i++)score[i][0] = -i;
+    for (int j = 0; j < col; j++)score[0][j] = -j;
 
     // recursive function
     for (int i = 1; i < row; i++)
@@ -40,8 +28,10 @@ int calcscore(string str1, string str2)
             // is match = 1, if not = -1
             int match = (str2[i - 1] == str1[j - 1]) ? 1 : -1;
             // score for diag, up, left
-            score[i][j] = max({score[i - 1][j - 1 + match], score[i - 1][j] - 1, score[i][j - 1] - 1} );
+            score[i][j] = max({score[i - 1][j - 1] + match, score[i - 1][j] - 1, score[i][j - 1] - 1});
+            // cout << score[i][j] ;
         }
+        
     }
 
     return score[row - 1][col - 1];
@@ -49,11 +39,11 @@ int calcscore(string str1, string str2)
 
 int main(int argc, char *argv[])
 {
-    string str1;
-    string str2;
+    string str1, str2;
     cin >> str1;
     cin >> str2;
-    int score = calcscore(str1, str2);
-    cout << score << endl;
+    int result = calcscore(str1, str2);
+    cout << result << endl;
+
     return 0;
 }
