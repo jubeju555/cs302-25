c33a5186ed83e1cf618107f69900ca82263284cf
judah <judahjosh@gmail.com>
2025-04-08 11:32:06 -0400

    dfs, needs checking

--- proj5/worddice.cpp
+++ proj5/worddice.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
-bool dfs(){
-vector<int> visited
-
+bool dfs(unordered_map<string, vector<string>> &graph, string start, string end, unordered_set<string> &visited)
+{
+  if (start == end)return true;
+  if (visited.find(start) != visited.end())return false;
+  visited.insert(start);
+  for (string &node : graph[start])
+  {
+    if (dfs(graph, node, end, visited))return true;
+    // cout << "Visiting " << node << endl;
+  }
+  return false;
 }
 
 int fulkerson(){
